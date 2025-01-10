51829d881dcb9da51d26cf8f7a11f4b1696091f8
Cyrile-Duquenoy <cyrileduquenoy@hotmail.fr>
2025-01-10 16:18:57 +0100

    Add files via upload

---
+++ C++/Mini Game/algo.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+// Ce .hpp sera utilisé pour implémenter les différents algo de déplacement.
+//
+// Débuter par un algo simplet qui déplace tout le monde de manière aléatoire
+//
+// Plus court chemin à parcourir pour les zombie
+// --> Dijkstra
+//
+//  Maximisation de la distance entre un joueur et les zombie
